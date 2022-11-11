5f4863d14f98e04cf2594dbb0c19aebf4eb4c9e8
nahco314 <nahco3_ta@yahoo.co.jp>
2022-11-11 20:44:47 +0900

    ツールを軽く整備・サンプルコードをコピー

---
+++ tools/py/main.py
@@ -0,0 +1,26 @@
+from typer import Typer
+
+import tools.py.run as run_
+from tools.py.console import console
+
+app = Typer()
+
+
+@app.command()
+def run(seed: int):
+    score = run_.run(seed)
+    console.print(f"score: {score}")
+
+
+@app.command()
+def mrun(seed_start: int, seed_end: int, multi_process: bool = True):
+    score_sum, score_avg = run_.multi_run(range(seed_start, seed_end), multi_process)
+    console.print(f"score_sum: {score_sum}, score_avg: {score_avg}")
+
+
+def main():
+    app()
+
+
+if __name__ == "__main__":
+    main()
---
+++ tools/py/run.py
@@ -0,0 +1,74 @@
+import re
+import subprocess
+from functools import partial
+from multiprocessing import Pool
+from time import perf_counter
+
+from rich.progress import Progress
+from rich.progress import Task
+from rich.progress import track
+
+from tools.py.console import console
+
+
+def run(seed: int, verbose: bool = False) -> int:
+    if verbose:
+        print("run", seed)
+
+    start = perf_counter()
+    res = subprocess.run(
+        f"cargo run --release --bin tester python ../ahc016/main.py < ./in/{seed:04}.txt > ./out/{seed:04}.txt",
+        shell=True,
+        capture_output=True,
+    )
+    time = perf_counter() - start
+    score = int(
+        re.match("Score = (\d+)", res.stderr.decode("utf-8").splitlines()[-1]).group(1)
+    )
+
+    with open(f"./score/{seed:04}.txt", "w") as f:
+        f.write(f"{score}")
+
+    if verbose:
+        print("end", seed, time)
+    return score
+
+
+def multi_run(
+    seed_range: range, multi_process=True, verbose: bool = False
+) -> tuple[int, float]:
+    score_sum = 0
+
+    if multi_process:
+        with Progress() as progress:
+            task = progress.add_task("Running...", total=len(seed_range))
+            with Pool(5) as p:
+                start = perf_counter()
+                results = []
+
+                args = []
+                for i in seed_range:
+                    args.append(i)
+
+                for result in p.imap(
+                    partial(run, verbose=verbose),
+                    args,
+                ):
+                    results.append(result)
+                    progress.advance(task)
+
+                score_sum = sum(results)
+                time = perf_counter() - start
+
+    else:
+        start = perf_counter()
+        for seed in track(seed_range):
+            score_sum += run(seed, verbose=verbose)
+        time = perf_counter() - start
+
+    score_avg = score_sum / len(seed_range)
+
+    with open(f"./score/0.txt", "w") as f:
+        f.write(f"{score_sum} {score_avg}")
+
+    return score_sum, score_avg
